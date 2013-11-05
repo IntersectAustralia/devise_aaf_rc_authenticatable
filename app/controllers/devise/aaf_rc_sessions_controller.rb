b9c26c5da6635a9b3928b13b867a5f404b5c9ab8
Gabriel E. Gasser Noblia <gabriel@intersect.org.au>
2013-11-05 15:34:23 +1100

    Moved Authentication back to the controller.

--- app/controllers/devise/aaf_rc_sessions_controller.rb
+++ app/controllers/devise/aaf_rc_sessions_controller.rb
@@ -1,6 +1,9 @@
 class Devise::AafRcSessionsController < Devise::SessionsController
   unloadable
-  prepend_before_filter :require_no_authentication, :only => [ :aaf_new, :aaf_create ]
+  prepend_before_filter :require_no_authentication, :only => [ :aaf_new]
+  prepend_before_filter :allow_params_authentication!, :only => :aaf_create
+  before_filter :authenticate_user!
+
   def aaf_new
     config = YAML.load(ERB.new(File.read(::Devise.aaf_rc_config || "#{Rails.root}/config/aaf_rc.yml")).result)[Rails.env]
 
@@ -9,12 +12,48 @@ class Devise::AafRcSessionsController < Devise::SessionsController
 
   def aaf_create
     #the authentication is taken care of by the strategy
+    jws = params["assertion"]
+
+    if jws
+      begin
+
+        config = YAML.load(ERB.new(File.read(::Devise.aaf_rc_config || "#{Rails.root}/config/aaf_rc.yml")).result)[Rails.env]
+
+        jwt = JSON::JWT.decode(jws.to_s, config['secret_token'])
+
+        aaf_host = config['aaf_rc_login_url'][/^https:\/\/[\w\.]+/] if config['aaf_rc_login_url']
+        aaf_host ||= "https://rapid.aaf.edu.au"
+
+        # In a complete app we'd also store and validate the jti value to ensure there is no replay attack
+        if jwt['iss'] == aaf_host && jwt['aud'] == config['hostname'] && Time.now > Time.at(jwt['nbf']) && Time.now < Time.at(jwt['exp'])
+          attributes = jwt['https://aaf.edu.au/attributes']
+          session[:attributes] = attributes
+          session[:jwt] = jwt
+        else
+          #TODO raise devise authentication error
+          #halt 500, "Audience or timings are invalid"
+        end
+      rescue Exception => e
+        puts e.message
+        #TODO raise devise authentication error
+        #halt 500, "Signature was invalid or JWT was otherwise erroneous"
+      end
+    else
+      #TODO raise devise authentication error
+      #halt 500, "JWS was not found in request"
+    end
+
+    redirect_to root_path
   end
 
   def destroy
     session.delete(:attributes)
     session.delete(:jwt)
     super
+  end
+
+  def extra_validations(attributes)
+    false
   end
 
 end
--- lib/devise_aaf_rc_authenticatable/strategy.rb
+++ lib/devise_aaf_rc_authenticatable/strategy.rb
@@ -6,11 +6,11 @@ module Devise
   module Strategies
     class AafRcAuthenticatable < Authenticatable
       def valid?
-        valid_for_aaf_rc_auth?
+        session['jwt'].present?
       end
 
       def authenticate!
-        if resource = mapping.to.authenticate_with_aaf(session[:attributes])
+        if resource = mapping.to.authenticate_with_aaf_rc(session[:attributes])
           if resource.active_for_authentication?
             success!(resource, :signed_in)
           else
@@ -19,41 +19,6 @@ module Devise
         else
 	        fail!(:invalid)
         end
-      end
-
-      def valid_for_aaf_rc_auth?
-
-        jws = params[:assertion]
-
-        if jws
-          begin
-
-            config = YAML.load(ERB.new(File.read(::Devise.aaf_rc_config || "#{Rails.root}/config/aaf_rc.yml")).result)[Rails.env]
-
-            jwt = JSON::JWT.decode(jws.to_s, config['server_token'])
-            aaf_host = config['aaf_rc_login_url'][/^https:\/\/[\w\.]+/] if config['aaf_rc_login_url']
-            aaf_host ||= "https://rapid.aaf.edu.au"
-
-            # In a complete app we'd also store and validate the jti value to ensure there is no replay attack
-            if jwt['iss'] == aaf_host && jwt['aud'] == config['hostname'] && Time.now > Time.at(jwt['nbf']) && Time.now < Time.at(jwt['exp'])
-              attributes = jwt['https://aaf.edu.au/attributes']
-              session[:attributes] = attributes
-              session[:jwt] = jwt
-            else
-              #TODO raise devise authentication error
-              #halt 500, "Audience or timings are invalid"
-            end
-          rescue Exception => e
-            #TODO raise devise authentication error
-            #halt 500, "Signature was invalid or JWT was otherwise erroneous"
-          end
-        else
-          #TODO raise devise authentication error
-          #halt 500, "JWS was not found in request"
-        end
-
-        # only try and authenticate if the AAF POST parameters are present
-        jws.present? && session[:attributes].present?
       end
     end
   end
